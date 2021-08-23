ede73bbba307687e7543bbd97f8fad6c59169cb2
Gustavo Madureira <gtmadureira@gmail.com>
2021-08-23 17:38:14 -0300

    Elliptic Curve Cryptography (ECC), with curve secp256k1.

--- ecc/secp256k1/secp256k1_jacobian.py
+++ ecc/secp256k1/secp256k1_jacobian.py
@@ -1,24 +1,24 @@
 """
-[ LICENSE ] ( GNU GPLv3 ) :
+[ LICENSE ]  ( GNU GPLv3 )  :
 
-SECP256K1 at Jacobian Form - Elliptic Curve Cryptography (ECC).
+SECP256K1 at Jacobian Form  -  Elliptic Curve Cryptography (ECC).
 Copyright (C) 2021  Gustavo Madureira
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program. If not, see https://www.gnu.org/licenses/ .
+You should have received a copy of the GNU General Public License along
+with this program. If not, see https://www.gnu.org/licenses/.
 
 
-[ ABOUT ] :
+[ ABOUT ]  :
 
 Elliptic Curve Cryptography (ECC).
 Module for asymmetric cryptography with elliptic curve SECP256K1, using
